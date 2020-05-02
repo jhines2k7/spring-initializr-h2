3bcdd408913be5d68bc943453d2116ffb528b42d
James Hines <jameshines10@gmail.com>
2020-05-02 17:50:44 +0000

    Initial commit

---
+++ src/main/java/com/james/hines/SpringInitializrH2.java
@@ -0,0 +1,13 @@
+package com.james.hines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitializrH2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInitializrH2.class, args);
+	}
+
+}
---
+++ src/test/java/com/james/hines/SpringInitializrH2Tests.java
@@ -0,0 +1,13 @@
+package com.james.hines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInitializrH2Tests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
